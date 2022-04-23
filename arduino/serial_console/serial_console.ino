d28d30f90b7c29b72f0cda0ff75e98f070685d51
ngomezcn <naimgomezcn@gmail.com>
2022-04-23 23:16:01 +0200

    serial receiver

--- arduino/serial_console/serial_console.ino
+++ arduino/serial_console/serial_console.ino
@@ -1,8 +1,37 @@
 
 #include <SoftwareSerial.h> 
 SoftwareSerial tx16s(0, 1); 
- String myStr = "";
 
+
+
+bool recv_str_status = false;
+
+String tmp_recv_str = "";
+String recv_str = "";
+
+void build_serial_string() {
+ 
+  if (tx16s.available() > 0) {
+    auto c_recv = tx16s.read();
+    tmp_recv_str += (char)c_recv;
+    if(c_recv == '\n'){
+        recv_str = tmp_recv_str;
+        recv_str_status = true;
+        tmp_recv_str = "";   
+    }
+  }
+}
+
+bool is_recv_str() {
+  bool tmp = recv_str_status;
+  recv_str_status = false;
+  return tmp;
+}
+
+String get_recv_string()
+{
+  return recv_str;
+}
 
 void setup() {
   Serial.begin(9600);
@@ -12,18 +41,18 @@ void setup() {
 }
 
 void loop() {
- auto xd = tx16s.available();
+ /*auto xd = tx16s.available();
  if (xd > 0) {
-  //Serial.print(tx16s.read());   // lee Bluetooth y envia a monitor serial de Arduino
-  //Serial.print(" xd: ");
-  //Serial.println(xd);
-
   auto r = tx16s.read();
   myStr += (char)r;
   if(r == '\n'){
       Serial.print(myStr);
       myStr = "";   
   }
+ } */
+  build_serial_string();
+ if(is_recv_str()) {
+  Serial.print(get_recv_string());
  }
 
  //tx16s.write("hola");
